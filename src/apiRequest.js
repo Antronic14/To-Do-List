38922abc4c2c8a692f041762ccf1d8a77bc7e3ae
Antronic <antronic14@gmail.com>
2025-06-20 21:49:44 +0530

    To-Do-List_React

---
+++ src/apiRequest.js
@@ -0,0 +1,14 @@
+const apiRequest = async(url='',optionObj=null,errMsg=null) =>{
+            try{
+                const response= await fetch(url,optionObj)
+                if(!response.ok) throw Error("Please Reload The App")
+
+            }catch(err){
+                errMsg=err.Message;
+            }finally{
+                return errMsg;
+            }
+        }
+
+
+export default apiRequest
